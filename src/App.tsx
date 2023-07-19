0c82d45fbf3f2cd82d60be7a911b11865de7004d
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2023-07-19 16:46:44 -0300

    Creates main skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <header>
+        <img />
+        <h1><span>to</span><span>do</span></h1>
+      </header>
+
+      <form>
+        <input />
+        <button type="submit" />
+      </form>
+
+      <section>
+        <div>
+          Tarefas criadas <span>5</span>
+        </div>
+        <div>
+          Concluídas <span>2 de 5</span>
+        </div>
+      </section>
+
+      <main>
+        <div>
+          <input type="checkbox" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, suscipit?</p>
+          <button />
+        </div>
+        <div>
+          <input type="checkbox" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, suscipit?</p>
+          <button />
+        </div>
+      </main>
+    </>
   )
 }
 
